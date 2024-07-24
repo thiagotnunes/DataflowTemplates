c1bbe81fd41f959475866e93cd3abaad6d16cc94
Thiago Nunes <thiagotnunes@google.com>
2024-07-24 21:23:06 +1000

    feat: adds PostgreSQL type mapping

---
+++ v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/reader/io/schema/typemapping/provider/PostgreSQLMappingProvider.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.source.reader.io.schema.typemapping.provider;
+
+import com.google.cloud.teleport.v2.source.reader.io.schema.typemapping.UnifiedTypeMapping;
+import com.google.cloud.teleport.v2.source.reader.io.schema.typemapping.provider.unified.UnifiedMappingProvider;
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public final class PostgreSQLMappingProvider {
+  private static final ImmutableMap<String, UnifiedTypeMapping> MAPPING =
+      ImmutableMap.<String, UnifiedMappingProvider.Type>builder()
+          .put("BIGINT", UnifiedMappingProvider.Type.LONG)
+          .put("BIGSERIAL", UnifiedMappingProvider.Type.LONG)
+          .put("BIT", UnifiedMappingProvider.Type.STRING)
+          .put("BIT VARYING", UnifiedMappingProvider.Type.STRING)
+          .put("BOOL", UnifiedMappingProvider.Type.BOOLEAN)
+          .put("BOOLEAN", UnifiedMappingProvider.Type.BOOLEAN)
+          .put("BOX", UnifiedMappingProvider.Type.STRING)
+          .put("BYTEA", UnifiedMappingProvider.Type.BYTES)
+          .put("CHAR", UnifiedMappingProvider.Type.STRING)
+          .put("CHARACTER", UnifiedMappingProvider.Type.STRING)
+          .put("CHARACTER VARYING", UnifiedMappingProvider.Type.STRING)
+          .put("CIDR", UnifiedMappingProvider.Type.STRING)
+          .put("CIRCLE", UnifiedMappingProvider.Type.STRING)
+          .put("DATE", UnifiedMappingProvider.Type.DATE)
+          .put("DECIMAL", UnifiedMappingProvider.Type.NUMBER)
+          .put("DOUBLE PRECISION", UnifiedMappingProvider.Type.DOUBLE)
+          .put("FLOAT4", UnifiedMappingProvider.Type.FLOAT)
+          .put("FLOAT8", UnifiedMappingProvider.Type.DOUBLE)
+          .put("INET", UnifiedMappingProvider.Type.STRING)
+          .put("INTEGER", UnifiedMappingProvider.Type.LONG)
+          .put("INT2", UnifiedMappingProvider.Type.LONG)
+          .put("INT4", UnifiedMappingProvider.Type.LONG)
+          .put("INT8", UnifiedMappingProvider.Type.LONG)
+          .put("INTERVAL", UnifiedMappingProvider.Type.TIME_INTERVAL)
+          .put("JSON", UnifiedMappingProvider.Type.JSON)
+          .put("JSONB", UnifiedMappingProvider.Type.JSON)
+          .put("LINE", UnifiedMappingProvider.Type.STRING)
+          .put("LSEG", UnifiedMappingProvider.Type.STRING)
+          .put("MACADDR", UnifiedMappingProvider.Type.STRING)
+          .put("MACADDR8", UnifiedMappingProvider.Type.STRING)
+          .put("MONEY", UnifiedMappingProvider.Type.STRING)
+          .put("NUMERIC", UnifiedMappingProvider.Type.NUMBER)
+          .put("PATH", UnifiedMappingProvider.Type.STRING)
+          .put("PG_LSN", UnifiedMappingProvider.Type.STRING)
+          .put("PG_SNAPSHOT", UnifiedMappingProvider.Type.STRING)
+          .put("POINT", UnifiedMappingProvider.Type.STRING)
+          .put("POLYGON", UnifiedMappingProvider.Type.STRING)
+          .put("REAL", UnifiedMappingProvider.Type.FLOAT)
+          .put("SERIAL2", UnifiedMappingProvider.Type.LONG)
+          .put("SERIAL4", UnifiedMappingProvider.Type.LONG)
+          .put("SERIAL8", UnifiedMappingProvider.Type.LONG)
+          .put("SMALLINT", UnifiedMappingProvider.Type.LONG)
+          .put("SMALLSERIAL", UnifiedMappingProvider.Type.LONG)
+          .put("TEXT", UnifiedMappingProvider.Type.STRING)
+          .put("TIME", UnifiedMappingProvider.Type.TIME)
+          .put("TIME WITHOUT TIME ZONE", UnifiedMappingProvider.Type.TIME)
+          .put("TIMESTAMP", UnifiedMappingProvider.Type.TIMESTAMP)
+          .put("TIMESTAMP WITHOUT TIME ZONE", UnifiedMappingProvider.Type.TIMESTAMP)
+          .put("TIMESTAMPTZ", UnifiedMappingProvider.Type.TIMESTAMP_WITH_TIME_ZONE)
+          .put("TIMESTAMP WITH TIME ZONE", UnifiedMappingProvider.Type.TIMESTAMP_WITH_TIME_ZONE)
+          .put("TIMETZ", UnifiedMappingProvider.Type.TIME_WITH_TIME_ZONE)
+          .put("TIME WITH TIME ZONE", UnifiedMappingProvider.Type.TIME_WITH_TIME_ZONE)
+          .put("TSQUERY", UnifiedMappingProvider.Type.STRING)
+          .put("TSVECTOR", UnifiedMappingProvider.Type.STRING)
+          .put("TXID_SNAPSHOT", UnifiedMappingProvider.Type.STRING)
+          .put("UUID", UnifiedMappingProvider.Type.STRING)
+          .put("VARBIT", UnifiedMappingProvider.Type.STRING)
+          .put("VARCHAR", UnifiedMappingProvider.Type.STRING)
+          .put("XML", UnifiedMappingProvider.Type.STRING)
+          .build()
+          .entrySet()
+          .stream()
+          .map(e -> Map.entry(e.getKey(), UnifiedMappingProvider.getMapping(e.getValue())))
+          .collect(ImmutableMap.toImmutableMap(Entry::getKey, Entry::getValue));
+
+  public static ImmutableMap<String, UnifiedTypeMapping> getMapping() {
+    return MAPPING;
+  }
+
+  private PostgreSQLMappingProvider() {}
+}
---
+++ v2/sourcedb-to-spanner/src/test/java/com/google/cloud/teleport/v2/source/reader/io/schema/typemapping/provider/unified/PostgreSQLMappingProviderTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.teleport.v2.source.reader.io.schema.typemapping.provider.unified;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.teleport.v2.source.reader.io.schema.typemapping.provider.PostgreSQLMappingProvider;
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import org.junit.Test;
+
+public class PostgreSQLMappingProviderTest {
+  @Test
+  public void testMySqlMappingProvider() {
+    Long[] testMods = {1L, 1L};
+    // We don't have a use case for arrays yet.
+    Long[] testArrayBounds = {};
+    ImmutableMap<String, String> stringifiedMapping =
+        PostgreSQLMappingProvider.getMapping().entrySet().stream()
+            .map(
+                e ->
+                    Map.entry(
+                        e.getKey(),
+                        e.getValue()
+                            .getSchema(testMods, testArrayBounds)
+                            .toString()
+                            .replaceAll("\\s+", "")))
+            .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+    assertThat(stringifiedMapping).isEqualTo(expectedMapping());
+  }
+
+  private ImmutableMap<String, String> expectedMapping() {
+    return ImmutableMap.<String, String>builder()
+        .put("BIGINT", "\"long\"")
+        .put("BIGSERIAL", "\"long\"")
+        .put("BIT", "\"string\"")
+        .put("BIT VARYING", "\"string\"")
+        .put("BOOL", "\"boolean\"")
+        .put("BOOLEAN", "\"boolean\"")
+        .put("BOX", "\"string\"")
+        .put("BYTEA", "\"bytes\"")
+        .put("CHAR", "\"string\"")
+        .put("CHARACTER", "\"string\"")
+        .put("CHARACTER VARYING", "\"string\"")
+        .put("CIDR", "\"string\"")
+        .put("CIRCLE", "\"string\"")
+        .put("DATE", "{\"type\":\"int\",\"logicalType\":\"date\"}")
+        .put("DECIMAL", "{\"type\":\"string\",\"logicalType\":\"number\"}")
+        .put("DOUBLE PRECISION", "\"double\"")
+        .put("FLOAT4", "\"float\"")
+        .put("FLOAT8", "\"double\"")
+        .put("INET", "\"string\"")
+        .put("INTEGER", "\"long\"")
+        .put("INT2", "\"long\"")
+        .put("INT4", "\"long\"")
+        .put("INT8", "\"long\"")
+        .put("INTERVAL", "{\"type\":\"long\",\"logicalType\":\"time-interval-micros\"}")
+        .put("JSON", "{\"type\":\"string\",\"logicalType\":\"json\"}")
+        .put("JSONB", "{\"type\":\"string\",\"logicalType\":\"json\"}")
+        .put("LINE", "\"string\"")
+        .put("LSEG", "\"string\"")
+        .put("MACADDR", "\"string\"")
+        .put("MACADDR8", "\"string\"")
+        .put("MONEY", "\"string\"")
+        .put("NUMERIC", "{\"type\":\"string\",\"logicalType\":\"number\"}")
+        .put("PATH", "\"string\"")
+        .put("PG_LSN", "\"string\"")
+        .put("PG_SNAPSHOT", "\"string\"")
+        .put("POINT", "\"string\"")
+        .put("POLYGON", "\"string\"")
+        .put("REAL", "\"float\"")
+        .put("SERIAL2", "\"long\"")
+        .put("SERIAL4", "\"long\"")
+        .put("SERIAL8", "\"long\"")
+        .put("SMALLINT", "\"long\"")
+        .put("SMALLSERIAL", "\"long\"")
+        .put("TEXT", "\"string\"")
+        .put("TIME", "{\"type\":\"long\",\"logicalType\":\"time-micros\"}")
+        .put("TIME WITHOUT TIME ZONE", "{\"type\":\"long\",\"logicalType\":\"time-micros\"}")
+        .put("TIMESTAMP", "{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"}")
+        .put(
+            "TIMESTAMP WITHOUT TIME ZONE",
+            "{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"}")
+        .put(
+            "TIMESTAMPTZ",
+            "{\"type\":\"record\",\"name\":\"timestampTz\",\"fields\":[{\"name\":\"timestamp\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"}},{\"name\":\"offset\",\"type\":{\"type\":\"int\",\"logicalType\":\"time-millis\"}}]}")
+        .put(
+            "TIMESTAMP WITH TIME ZONE",
+            "{\"type\":\"record\",\"name\":\"timestampTz\",\"fields\":[{\"name\":\"timestamp\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"}},{\"name\":\"offset\",\"type\":{\"type\":\"int\",\"logicalType\":\"time-millis\"}}]}")
+        .put(
+            "TIMETZ",
+            "{\"type\":\"record\",\"name\":\"timeTz\",\"fields\":[{\"name\":\"time\",\"type\":{\"type\":\"long\",\"logicalType\":\"time-micros\"}},{\"name\":\"offset\",\"type\":{\"type\":\"int\",\"logicalType\":\"time-millis\"}}]}")
+        .put(
+            "TIME WITH TIME ZONE",
+            "{\"type\":\"record\",\"name\":\"timeTz\",\"fields\":[{\"name\":\"time\",\"type\":{\"type\":\"long\",\"logicalType\":\"time-micros\"}},{\"name\":\"offset\",\"type\":{\"type\":\"int\",\"logicalType\":\"time-millis\"}}]}")
+        .put("TSQUERY", "\"string\"")
+        .put("TSVECTOR", "\"string\"")
+        .put("TXID_SNAPSHOT", "\"string\"")
+        .put("UUID", "\"string\"")
+        .put("VARBIT", "\"string\"")
+        .put("VARCHAR", "\"string\"")
+        .put("XML", "\"string\"")
+        .build()
+        .entrySet()
+        .stream()
+        .map(e -> Map.entry(e.getKey(), e.getValue().replaceAll("\\s+", "")))
+        .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+  }
+}
